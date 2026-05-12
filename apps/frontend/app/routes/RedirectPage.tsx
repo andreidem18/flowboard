b4a7e842fe2db39cd9cf25b7b3bb285866c8e2f5
andreidem18 <andres.david.mm@hotmail.com>
2026-05-12 11:09:51 -0500

    chore: avoid error when entering to root page

--- apps/frontend/app/routes/RedirectPage.tsx
+++ apps/frontend/app/routes/RedirectPage.tsx
@@ -1,5 +1,12 @@
 import { Navigate } from "react-router";
+import { useAuth } from "~/features/auth/hooks";
 
 export default function RedirectPage() {
+  const { isAuthenticated, loading } = useAuth();
+
+  if (loading) return <></>;
+
+  if (!isAuthenticated) return <Navigate to="/auth" />;
+
   return <Navigate to="/app/dashboard" />;
 }
