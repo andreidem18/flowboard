253b9368076b7bb59aa3bfa9a7398dd531158d58
andreidem18 <andres.david.mm@hotmail.com>
2026-05-18 17:51:25 -0500

    chore: improve logging

--- apps/backend/src/lib/logger.ts
+++ apps/backend/src/lib/logger.ts
@@ -1,10 +1,24 @@
 import pino from "pino";
+import { env } from "./env";
 
-export const logger = pino({
-  transport: {
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-    },
-  },
-});
+const isProduction =
+  env.NODE_ENV === "production" || process.env.npm_lifecycle_event === "start";
+
+export const logger = pino(
+  isProduction
+    ? {
+        level: "info",
+        timestamp: pino.stdTimeFunctions.isoTime,
+      }
+    : {
+        level: "info",
+        transport: {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+            translateTime: "SYS:yyyy-mm-dd HH:MM:ss.l",
+            ignore: "pid,hostname",
+          },
+        },
+      },
+);
